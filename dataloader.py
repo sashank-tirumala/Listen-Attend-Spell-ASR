7a8ac67e168eb064eed660a772c992e9356b93f2
sashank-tirumala <stsashank6@gmail.com>
2022-04-24 17:56:47 -0400

    More changes to run the training on the cluster

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import pandas as pd
 import numpy as np
 # import Levenshtein as lev
 import torch
---
+++ load_data.bash
@@ -0,0 +1,6 @@
+export KAGGLE_USERNAME="sashanktirumala"
+export KAGGLE_KEY="4b4c6fed7f1c86a94a5c1ec97cf34a35"
+
+kaggle competitions download -c 11-785-s22-hw4p2
+
+unzip -qq 11-785-s22-hw4p2.zip
