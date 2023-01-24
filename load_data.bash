56adf5fc2d7dd7d46ec7e93674fc38077de327ca
sashank tirumala <stsashank6@gmail.com>
2023-01-24 15:33:21 -0500

    New data loading commands

--- load_data.bash
+++ load_data.bash
@@ -1,9 +1,3 @@
-#export KAGGLE_USERNAME="sashanktirumala"
-#export KAGGLE_KEY="4b4c6fed7f1c86a94a5c1ec97cf34a35"
-
-#kaggle competitions download -c 11-785-s22-hw4p2
-
-#unzip -qq 11-785-s22-hw4p2.zip
 pip install gdown
 gdown https://drive.google.com/uc?id=1RbjTP3wJsR0g4XlbsECkRjJB7A6md06X
 unzip -qq LAS-Dataset.zip
